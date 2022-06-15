2e92f1d9fad0082fd9ad38bdd8c948f687a70313
Chimpook Virtuch <chimpook@gmail.com>
2022-06-15 11:41:14 +0200

    Examples

--- README.md
+++ README.md
@@ -1,2 +1,35 @@
 # cataas-app
 Example app for implementing the cataas-api-php library.
+
+// To get the URL of a cat:
+$url = $cataas->getUrl();
+
+// To download a cat:
+$cataas->get('randomCat.png');
+
+// To get a cat and output it to STDOUT
+$cataas->get();
+
+// Usage variants from cataas.com:
+
+$cataas->tag('cute')->html()->get();
+$cataas->tag('cute')->get();
+$cataas->gif()->get();
+$cataas->says('Hello, human!')->get();
+$cataas->says('Hello, human!')->size(22)->color('green')->get();
+$cataas->type('sq')->get();
+$cataas->filter('negative')->get();
+$cataas->width(360)->get();
+$cataas->height(240)->get();
+$cataas->gif()->says('Hello!')->filter('sepia')->color('orange')->size(40)->type('or')->get();
+
+$cataas->api()->cats()->tags('cute,fail')->skip(0)->limit(10)->get();
+$cataas->api()->tags()->get();
+
+// Examples of console oneliners
+
+// Get a cat and flip it upside down
+php -r 'require "Cataas.php"; echo (new Cataas)->get();' | cat | convert - -flip flipped_cat.png
+
+// Get a cat and frame it with a tomato frame
+php -r 'require "Cataas.php"; echo (new Cataas)->get();' | cat | convert - -bordercolor Tomato -border 10%x10% framed_cat.png
--- src/cataas-app/index.php
+++ src/cataas-app/index.php
@@ -4,52 +4,8 @@ require_once 'Cataas.php';
 
 $cataas = new Cataas();
 
-//$url = $cataas->getUrl();
-//$url = $cataas->json()->getUrl();
-//$url = $cataas->tag('cute')->getUrl();
-//$url = $cataas->gif()->getUrl();
-//$url = $cataas->says('Hello, human!')->getUrl();
-//$url = $cataas->says('Hello, human!')->size(22)->color('green')->getUrl();
-//$url = $cataas->type('sq')->getUrl();
-//$url = $cataas->filter('negative')->getUrl();
-//$url = $cataas->width(360)->getUrl();
-//$url = $cataas->height(240)->getUrl();
-//$url = $cataas->gif()->says('Hello!')->filter('sepia')->color('orange')->size(40)->type('or')->getUrl();
-
-//$url = $cataas->api()->cats()->tags('cute,fail')->skip(0)->limit(10)->getUrl();
-//$url = $cataas->api()->tags()->getUrl();
-/*
-echo <<<HTML
-<body style="margin: 0px; background: #0e0e0e; height: 100%">
-    <div style="color:yellow;">{$url}</div>
-    <!--<img style="display: block;-webkit-user-select: none;margin: auto;background-color: hsl(0, 0%, 90%);transition: background-color 300ms;" src="{$url}">-->
-</body>
-HTML;
-*/
-
-
 try {
-    //$cataas->tag('cute')->html()->get();
-    //$cataas->get('/var/www/cataas-app/images/randomCat.png');
-    //$cataas->tag('cute')->get('/var/www/cataas-app/images/randomCat.png');
-    //$cataas->gif()->get('/var/www/cataas-app/images/randomCat.png');
-    //$cataas->says('Hello, human!')->get('/var/www/cataas-app/images/randomCat.png');
-    //$cataas->says('Hello, human!')->size(22)->color('green')->get('/var/www/cataas-app/images/randomCat.png');
-    //$cataas->type('sq')->get('/var/www/cataas-app/images/randomCat.png');
-    //$cataas->filter('negative')->get('/var/www/cataas-app/images/randomCat.png');
-    //$cataas->width(360)->get('/var/www/cataas-app/images/randomCat.png');
-    //$cataas->height(240)->get('/var/www/cataas-app/images/randomCat.png');
-    //$cataas->gif()->says('Hello!')->filter('sepia')->color('orange')->size(40)->type('or')->get('/var/www/cataas-app/images/randomCat.png');
     
-    //$cataas->api()->cats()->tags('cute,fail')->skip(0)->limit(10)->get();
-    //$cataas->api()->tags()->get();
-/*
-    echo <<<HTML
-    <body style="margin: 0px; background: #0e0e0e; height: 100%">
-        <img style="display: block;-webkit-user-select: none;margin: auto;background-color: hsl(0, 0%, 90%);transition: background-color 300ms;" src="/images/randomCat.png">
-    </body>
-    HTML;
-*/    
 } catch (Exception $e) {
     echo $e;
 }
